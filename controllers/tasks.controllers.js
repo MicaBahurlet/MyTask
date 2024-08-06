9e8ac9890ff9ff13a378dd4d1207963f283ee20f
Micaela Bahurlet <micabahurletgo@gmail.com>
2024-08-06 03:56:41 -0300

    start routes back

---
+++ controllers/tasks.controllers.js
@@ -0,0 +1,20 @@
+const getTasks = (req, res) => {
+    res.send ("Obteniendo tareas");
+
+}
+const getTask = (req, res) => {
+    res.send ("obteniendo tarea");
+
+}
+const createTasks = (req, res) => {
+    res.send ("Creando tareas");
+
+}
+const updateTasks = (req, res) => {
+    res.send ("Actualizando tareas");
+
+}
+const deleteTasks = (req, res) => {
+    res.send ("Borrando tarea");
+
+}
