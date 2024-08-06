042046f3ca8a23a5766017338cd8b6e76e0f6123
Micaela Bahurlet <micabahurletgo@gmail.com>
2024-08-06 16:14:45 -0300

    finish routs to taks

--- controllers/tasks.controllers.js
+++ controllers/tasks.controllers.js
@@ -1,20 +1,20 @@
-const getTasks = (req, res) => {
+export const getTasks = (req, res) => {
     res.send ("Obteniendo tareas");
 
 }
-const getTask = (req, res) => {
+export const getTask = (req, res) => {
     res.send ("obteniendo tarea");
 
 }
-const createTasks = (req, res) => {
+export const createTasks = (req, res) => {
     res.send ("Creando tareas");
 
 }
-const updateTasks = (req, res) => {
-    res.send ("Actualizando tareas");
+export const updateTasks = (req, res) => {
+    res.send ("Actualizando tarea");
 
 }
-const deleteTasks = (req, res) => {
-    res.send ("Borrando tarea");
+export const deleteTasks = (req, res) => {
+    res.send ("Eliminando tarea");
 
 }
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
-import {PORT} from "./server/config.js";
+import { PORT } from "./server/config.js";
 
 import indexRoutes from "./server/routes/index.routes.js";
+import taskRoutes from "./server/routes/tasks.routes.js";
 
 const app = express();
 
-app.use (indexRoutes);
-app.listen (PORT)
-console.log(`Server running on port ${PORT}`)
+app.use(indexRoutes);
+app.use(taskRoutes);
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
