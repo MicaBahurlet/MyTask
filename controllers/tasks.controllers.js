8005eecaedefe4a505dd25c5fb60bef2db0ee086
Micaela Bahurlet <micabahurletgo@gmail.com>
2024-08-07 18:44:06 -0300

    finish get tasks endpoints

--- controllers/tasks.controllers.js
+++ controllers/tasks.controllers.js
@@ -1,10 +1,23 @@
 import { pool } from "../server/db.js";
 
-export const getTasks = (req, res) => {
-  res.send("Obteniendo tareas");
+export const getTasks = async (req, res) => {
+  const [result] = await pool.query(
+    "SELECT * FROM tasks ORDER BY createAt ASC"
+  );
+  console.log(result);
+  res.json(result);
 };
-export const getTask = (req, res) => {
-  res.send("obteniendo tarea");
+
+export const getTask = async (req, res) => {
+  const [result] = await pool.query("SELECT * FROM tasks WHERE id = ?", [
+    req.params.id,
+  ]); //selecciona todas las tareas en las que coincida el id que te estoy pasando en la querry
+
+  if (result.length === 0) {
+    return res.status(404).json({ message: "Tarea no encontrada" });
+  }
+
+  res.json(result [0]); // quiero que solo me devuelva el primero del array
 };
 //Tipié mal en la creación de la tabla, es tittle. title no existe.
 export const createTask = async (req, res) => {
