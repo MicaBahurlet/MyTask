bc6053ff36185e52622caf77cf905b56be104983
Micaela Bahurlet <micabahurletgo@gmail.com>
2024-09-05 08:32:46 -0300

    add variables de entorno

--- server/config.js
+++ server/config.js
@@ -1,2 +1,12 @@
 
-export const PORT = 8080;
+// export const PORT = 8080;
+
+export const PORT = process.env.PORT || 8080 // para el puerto que me de el servidor
+
+//para evitar que en produccion se sepan estos datos
+
+export const DB_HOST = process.env.DB_HOST || "localhost" // para la base de datos
+export const DB_USER = process.env.DB_USER || "root"
+export const DB_PASSWORD = process.env.DB_PASSWORD || "Mytask1"
+export const DB_NAME = process.env.DB_NAME || "tasksdb"
+export const DB_PORT = process.env.DB_PORT || "3306"
--- server/db.js
+++ server/db.js
@@ -1,11 +1,27 @@
 import { createPool } from "mysql2/promise";
+import {
+    DB_HOST,
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME,
+    DB_PORT
+} from "./config.js"
+
 
 export const pool = createPool({
-    host: "localhost",
-    user: "root",
-    password: "Mytask1",
-    port: 3306,
-    database: "tasksdb"
-});
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    database: DB_NAME
+})
+
+// export const pool = createPool({
+//     host: "localhost",
+//     user: "root",
+//     password: "Mytask1",
+//     port: 3306,
+//     database: "tasksdb"
+// });
 
 
